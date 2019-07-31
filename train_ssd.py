6c7d847dfbfe4b260936959cfd3d79a508bbb747
Pranoy R <pranoyalkr@gmail.com>
2019-07-31 17:40:52 +0530

    updated for cuda

--- train_ssd.py
+++ train_ssd.py
@@ -132,6 +132,10 @@ def train(loader, net, encoder, decoder, criterion, optimizer, device, debug_ste
         # timesteps, batch,  channel, height, width
         # torch.Size([2, 24, 512, 38, 38])
         out_enc_23, out_enc_final = encoder(videos)
+
+        out_enc_23 = out_enc_23.to(device)
+        out_enc_final = out_enc_final.to(device)
+        
         out_dec_23, out_dec_final  = decoder([out_enc_23, out_enc_final])
 
 
