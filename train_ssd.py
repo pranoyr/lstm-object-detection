f6460551f0ba9b789ef286b61fa3d1c8540d5ba4
Pranoy R <pranoyalkr@gmail.com>
2019-07-31 17:19:03 +0530

    removed val

--- train_ssd.py
+++ train_ssd.py
@@ -379,6 +379,6 @@ if __name__ == '__main__':
             #     f"Validation Regression Loss {val_regression_loss:.4f}, " +
             #     f"Validation Classification Loss: {val_classification_loss:.4f}"
             #)
-            model_path = os.path.join(args.checkpoint_folder, f"{args.net}-Epoch-{epoch}-Loss-{val_loss}.pth")
+            model_path = os.path.join(args.checkpoint_folder, f"{args.net}-Epoch-{epoch}.pth")
             net.save(model_path)
             logging.info(f"Saved model {model_path}")
