5373e581b2e6def5b674a951e81958f6fb624b49
Pranoy R <pranoyalkr@gmail.com>
2019-07-31 19:25:33 +0530

    updated for cuda

--- vision/nn/conv_lstm.py
+++ vision/nn/conv_lstm.py
@@ -42,6 +42,8 @@ class ConvLSTMCell(nn.Module):
     def forward(self, input_tensor, cur_state):
         
         h_cur, c_cur = cur_state
+
+        print(h_cur.device)
         
         combined = torch.cat([input_tensor, h_cur], dim=1)  # concatenate along channel axis
         
