fd02ad997adf8968c769c8994286113d5be510ee
Pranoy R <pranoyalkr@gmail.com>
2019-07-31 18:38:53 +0530

    updated for cuda

--- vision/ssd/lstm_vgg_ssd.py
+++ vision/ssd/lstm_vgg_ssd.py
@@ -143,12 +143,12 @@ class DecoderRNN(nn.Module):
 		x_RNN_23 , x_RNN_final = x_RNN[0], x_RNN[1]
 		
 		#self.ConvLSTM.flatten_parameters()
-		RNN_out_23, _ = self.ConvLSTM_23(x_RNN_23, None)  
+		RNN_out_23, _ = self.ConvLSTM_23(x_RNN_23)  
 		""" h_n shape (n_layers, batch, hidden_size), h_c shape (n_layers, batch, hidden_size) """ 
 		""" None represents zero initial hidden state. RNN_out has shape=(time_step, batch, output_size) """
 
 		#self.ConvLSTM.flatten_parameters()
-		RNN_out_final, _ = self.ConvLSTM_final(x_RNN_final, None)  
+		RNN_out_final, _ = self.ConvLSTM_final(x_RNN_final)  
 		""" h_n shape (n_layers, batch, hidden_size), h_c shape (n_layers, batch, hidden_size) """ 
 		""" None represents zero initial hidden state. RNN_out has shape=(time_step, batch, output_size) """
 	
