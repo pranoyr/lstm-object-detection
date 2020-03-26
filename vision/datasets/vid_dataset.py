ef79a01f6d446182ca64f35db3b065dddabdb220
Pranoy R <pranoyalkr@gmail.com>
2020-03-26 20:36:55 +0530

    updated queue

--- vision/datasets/vid_dataset.py
+++ vision/datasets/vid_dataset.py
@@ -75,9 +75,9 @@ class VIDDataset:
 				video_boxes.append(boxes)
 				video_labels.append(labels)
 
-		video = torch.stack(video)[:5]
-		video_boxes = torch.stack(video_boxes)[:5]
-		video_labels = torch.stack(video_labels)[:5]
+		video = torch.stack(video)
+		video_boxes = torch.stack(video_boxes)
+		video_labels = torch.stack(video_labels)
 
 		# print("_______")
 		# print(video.size())
