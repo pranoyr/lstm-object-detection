0c0a7c9959c45e475c347053e999144c2d26614b
Pranoy R <pranoyalkr@gmail.com>
2020-03-26 17:22:14 +0530

    updated queue

--- train_ssd1.py
+++ train_ssd1.py
@@ -267,7 +267,7 @@ if __name__ == '__main__':
             # label_file = os.path.join(
             #     args.checkpoint_folder, "voc-model-labels.txt")
             # store_labels(label_file, dataset.class_names)
-            # num_classes = len(dataset.class_names)
+            num_classes = len(dataset.class_names)
         elif args.dataset_type == 'open_images':
             dataset = OpenImagesDataset(dataset_path,
                                         transform=train_transform, target_transform=target_transform,
