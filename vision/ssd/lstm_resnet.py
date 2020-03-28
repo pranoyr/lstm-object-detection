95de8e546112fb75522ddfe70f3c9f19107e245c
Pranoy R <pranoyalkr@gmail.com>
2020-03-28 21:14:35 +0530

    3,4,5 lstm layers updated

--- vision/ssd/lstm_resnet.py
+++ vision/ssd/lstm_resnet.py
@@ -3,12 +3,12 @@ import torch
 import numpy as np
 from typing import List, Tuple
 import torch.nn.functional as F
-# from ..utils import box_utils
-import box_utils
+from ..utils import box_utils
+# import box_utils
 from torch.nn import Conv2d, Sequential, ModuleList, ReLU, BatchNorm2d
-from conv_lstm import ConvLSTMCell
-from conv_lstm import BottleNeckLSTM
-import mobilenetv1_ssd_config as config
+from .conv_lstm import ConvLSTMCell
+from .conv_lstm import BottleNeckLSTM
+# import mobilenetv1_ssd_config as config
 from torchvision.models import resnet101
 
 # borrowed from "https://github.com/marvis/pytorch-mobilenet"
