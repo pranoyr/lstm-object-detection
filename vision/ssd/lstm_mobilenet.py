8a90d16f65f0a907f1601ba86f4da7d2680bc811
Pranoy R <pranoyalkr@gmail.com>
2020-03-31 22:24:21 +0530

    reverted to pranoy

--- vision/ssd/lstm_mobilenet.py
+++ vision/ssd/lstm_mobilenet.py
@@ -127,7 +127,7 @@ class MobileNetLSTM(nn.Module):
 			conv_dw_1(inp=512, oup=6 * 4, kernel_size=3, padding=1),
 			conv_dw_1(inp=1024, oup=6 * 4, kernel_size=3, padding=1),
 			conv_dw_1(inp=256, oup=6 * 4, kernel_size=3, padding=1),
-			conv_dw_1(inp=16, oup=6 * 4, kernel_size=3, padding=1),
+			conv_dw_1(inp=64, oup=6 * 4, kernel_size=3, padding=1),
 			conv_dw_1(inp=16, oup=6 * 4, kernel_size=3, padding=1),
 			conv_dw_1(inp=16, oup=6 * 4, kernel_size=3, padding=1),
 		])
@@ -136,7 +136,7 @@ class MobileNetLSTM(nn.Module):
 			conv_dw_1(inp=512, oup=6 * num_classes, kernel_size=3, padding=1),
 			conv_dw_1(inp=1024, oup=6 * num_classes, kernel_size=3, padding=1),
 			conv_dw_1(inp=256, oup=6 * num_classes, kernel_size=3, padding=1),
-			conv_dw_1(inp=16, oup=6 * num_classes, kernel_size=3, padding=1),
+			conv_dw_1(inp=64, oup=6 * num_classes, kernel_size=3, padding=1),
 			conv_dw_1(inp=16, oup=6 * num_classes, kernel_size=3, padding=1),
 			conv_dw_1(inp=16, oup=6 * num_classes, kernel_size=3, padding=1),
 		])
