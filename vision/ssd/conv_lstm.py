6f12cfcebbc2a3512e0b20069d311789a80db9bf
Pranoy R <pranoyalkr@gmail.com>
2020-03-31 21:27:36 +0530

    reverted to pranoy

--- vision/ssd/conv_lstm.py
+++ vision/ssd/conv_lstm.py
@@ -36,7 +36,8 @@ class BottleNeckLSTM(nn.Module):
         self.hidden_size = hidden_size
         self.bottleneck_gate = nn.Sequential(
             nn.Conv2d(input_size + hidden_size, hidden_size,
-                    KERNEL_SIZE, padding=PADDING))
+                    KERNEL_SIZE, padding=PADDING),
+            nn.BatchNorm2d(hidden_size))
         self.Gates = conv_dw(hidden_size, 4 * hidden_size,
                              KERNEL_SIZE, padding=PADDING)
         self.hidden_state = None
